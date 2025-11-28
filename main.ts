cee547faf142c82237f5614c106d330bc36d02e8
Saad <sa3dmalaeb@gmail.com>
2025-11-29 01:27:14 +0200

    Add /list endpoint

--- main.ts
+++ main.ts
@@ -6,4 +6,11 @@ app.get("/", (c) => {
   return c.text("Hi Mom!");
 });
 
+app.get("/list", (c) => {
+  return c.json([
+    { name: "Saad", age: 33 },
+    { name: "Jad", age: 30 },
+  ]);
+});
+
 Deno.serve(app.fetch);
